380646f1774f14af8f0f0d4b372f8ee43f189baf
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2024-11-01 17:43:44 -0300

    Implementando o conteudo do dia 1

---
+++ dia-1/typescript/index.js
@@ -0,0 +1,5 @@
+function add(number1, number2) {
+    return number1 + number2;
+}
+var result = add(5, 8);
+console.log(result);
---
+++ dia-1/typescript/index.ts
@@ -0,0 +1,6 @@
+function add(number1: number, number2: number) {
+  return number1 + number2;
+}
+
+let result = add(5, 8);
+console.log(result);
