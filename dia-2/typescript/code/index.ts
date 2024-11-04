7787608c5083008699e028ee0059f213f8057895
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2024-11-04 17:57:19 -0300

    Implementando o conteudo do dia 2, ate a aula de tuples

---
+++ dia-2/typescript/code/index.ts
@@ -0,0 +1,21 @@
+// tipo number
+let price = 40;
+
+// tipo string
+let item = "apple";
+
+// tipo boolean
+let inStore = true;
+
+// tipo any
+let value;
+value = 10;
+value = "Matheus";
+
+//tipo array
+let prices: number[] = [10, 20, 30, 40, 60];
+
+//tipo tuple
+let itens: [string, number, boolean] = ["Apple", 3, true];
+
+itens[1].toString;
---
+++ dia-2/typescript/prod/index.js
@@ -0,0 +1,16 @@
+"use strict";
+// tipo number
+let price = 40;
+// tipo string
+let item = "apple";
+// tipo boolean
+let inStore = true;
+// tipo any
+let value;
+value = 10;
+value = "Matheus";
+//tipo array
+let prices = [10, 20, 30, 40, 60];
+//tipo tuple
+let itens = ["Apple", 3, true];
+itens[1].toString;
